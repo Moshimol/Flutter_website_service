60a442fd62bf2cfd321c730c3a5d9cf2724603e5
lushitong <lushitong@qq.com>
2020-06-15 00:21:35 +0800

    添加反馈接口
    Signed-off-by: lushitong <lushitong@qq.com>

--- flutter_website_service/main.dart
+++ flutter_website_service/main.dart
@@ -35,10 +35,16 @@ void hadleGet(HttpRequest request) {
     request.response..statusCode = HttpStatus.ok..write(json.encode(products))..close();
   } else if (action == 'get_news') {
     request.response..statusCode = HttpStatus.ok..write(json.encode(news))..close();
+  } else if (action == "company") {
+    var msg = request.uri.queryParameters['msg'];
+    print("客户留言为 $msg");
+    request.response..statusCode = HttpStatus.ok..write('客户留言为 $msg')..close();
   }
 
 }
 
 void hadlePOST(HttpRequest request) {
-  
+  // 个人反馈
+
+
 }
